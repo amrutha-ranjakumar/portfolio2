460c914226d94418c5c9367671b77858b2f577b9
amrutha-ranjakumar <147486960+amrutha-ranjakumar@users.noreply.github.com>
2024-03-27 13:38:41 +0530

    portfolio React Project

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './contact.css'
 
 export const Contact = () => {
   const form = useRef();
@@ -25,37 +26,23 @@ export const Contact = () => {
 
   return (
     <div className='container'>
-      <form ref={form} onSubmit={sendEmail} style={{ marginLeft: '-100px',marginTop:"100px" }}>
+      <form ref={form} onSubmit={sendEmail} style={{ marginTop: '100px' }}>
         <Row>
-          <Col>
-            <h3 className='text-warning mt-5 mb-2 ms-5 d-flex justify-content-center align-items-center'>
-              CONTACT ME
-            </h3>
+          <Col xs={12} md={6} className="mx-auto">
+            <h3 className='text-warning mt-5 mb-2 text-center'>CONTACT ME</h3>
             <Link to='/'>
-              <button className='btn btn-warning' style={{ width: '40px',height:"50px", marginLeft: '-700px', backgroundColor: 'orange' }}>
-                <i className='fa-solid fa-angle-left'></i> 
+              <button className='btn btn-warning mb-3' style={{ width: '40px', height: '50px', backgroundColor: 'orange' }}>
+                <i className='fa-solid fa-angle-left'></i>
               </button>
             </Link>
-            <div style={{ color: 'white'}}>
-              <label htmlFor='name' style={{ marginLeft: '-200px', color: 'yellow' }}>
-                Name:
-              </label>
-              <input type='text' className='form-control' style={{ width: '200%', marginLeft: '30px', backgroundColor: 'orange' }} name='from_name' />
-              <br />
-              <br />
-              <label htmlFor='email' style={{ marginLeft: '-200px', color: 'yellow' }}>
-                Email
-              </label>
-              <input type='email' className='form-control' style={{ width: '200%', marginLeft: '30px', backgroundColor: 'orange' }} name='from_email' />
-              <br />
-              <br />
-              <label htmlFor='message' style={{ marginLeft: '-180px', color: 'yellow' }}>
-                Message
-              </label>
-              <textarea name='message' className='form-control' style={{ width: '200%', height: '30px', marginLeft: '30px', backgroundColor: 'orange' }} />
-              <br />
-              <br />
-              <input type='submit' className='form-control btn btn-warning' style={{ width: '200%', marginLeft: '30px', borderRadius: '500px' }} value='Send' />
+            <div style={{ color: 'white' }}>
+              <label htmlFor='name' style={{ color: 'yellow' }}>Name:</label>
+              <input type='text' className='form-control mb-3' style={{ backgroundColor: 'orange' }} name='from_name' />
+              <label htmlFor='email' style={{ color: 'yellow' }}>Email</label>
+              <input type='email' className='form-control mb-3' style={{ backgroundColor: 'orange' }} name='from_email' />
+              <label htmlFor='message' style={{ color: 'yellow' }}>Message</label>
+              <textarea name='message' className='form-control mb-3' style={{ height: '100px', backgroundColor: 'orange' }} />
+              <input type='submit' className='form-control btn btn-warning' style={{ borderRadius: '500px' }} value='Send' />
             </div>
           </Col>
         </Row>
